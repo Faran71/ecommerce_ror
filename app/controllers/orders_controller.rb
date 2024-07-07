dcbdeecf3e322b7e6ba9fc1121083252e98a61fd
Muhammad Faran Sarwar <muhammadfaran01.mfs@gmail.com>
2024-07-07 16:19:22 +0100

    added endpoint for user to make an order, deducting from product quantity and users wallet

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,55 @@
 class OrdersController < ApplicationController
 
+  # GET /orders
   def index
     @orders = Order.all
     render json: @orders
   end
 
+  # GET /orders/filter_by_user/{:user_id}
   def filter_by_user
     user = User.find(params[:user_id])
     @orders = user.order
     render json: @orders
+  end
+
+  # POST /orders
+  def create
+    @order = Order.new(order_params)
+
+    cost = @order.product.price * @order.quantity_sold
+
+    if(@order.user.wallet >= cost) && (@order.product.quantity >= @order.quantity_sold)
+      if @order.save
+        update_product_quantities(@order)
+        update_user_wallet(@order)
+        render json: @order, status: :created, location: @order
+      else
+        render json: @order.errors, status: :unprocessable_entity
+      end
+    else
+      render json: @order.errors, status: :unauthorized
+    end
+  end
+
+
+
+  private
+
+  def order_params
+    params.require(:order).permit(:user_id, :product_id, :quantity_sold)
+    # params.require(:user_id, :product_id, :quantity_sold)
+  end
+
+  def update_product_quantities(order)
+    product = order.product
+    product.quantity -= order.quantity_sold
+    product.save
+  end
+
+  def update_user_wallet(order)
+    user = order.user
+    user.wallet -= (order.quantity_sold * order.product.price)
+    user.save
   end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,16 @@
 class ProductsController < ApplicationController
 
+  # GET /products
   def index
     @products = Product.all
     render json: @products
   end
+
+  # GET /products/{:id}
+  def show
+    @product = Product.find(params[:id])
+    render json: @product
+  end
+
+
 end
