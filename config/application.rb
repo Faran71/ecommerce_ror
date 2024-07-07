7f8a01a66b82ff34c819895fc5bee5b4ef51e971
Muhammad Faran Sarwar <muhammadfaran01.mfs@gmail.com>
2024-07-07 20:20:07 +0100

    same as previous commit

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,16 @@ module EcommerceRor
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://localhost:3001' # Update with your frontend URL
+
+        resource '*',
+          headers: :any,
+          methods: [:get, :post, :put, :patch, :delete, :options, :head],
+          credentials: true # Allow cookies to be sent if needed
+      end
+    end
   end
 end
