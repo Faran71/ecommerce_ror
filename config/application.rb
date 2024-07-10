e75900d16fdd293269072d692983bbd13678e46e
Muhammad Faran Sarwar <muhammadfaran01.mfs@gmail.com>
2024-07-10 22:54:25 +0100

    removed "

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,7 @@ module EcommerceRor
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http://localhost:3000', 'http://18.169.50.207:3000/' # Update with your frontend URL
+        origins 'http://localhost:3000', 'http://18.169.50.207:3000' # Update with your frontend URL
 
         resource '*',
           headers: :any,
